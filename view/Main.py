8bb1b3156c89111b063532c8689b6f7b04168dc7
xandy <>
2025-02-03 13:45:30 -0400

    /feature: Create MVC model and add Base Frames

---
+++ view/Main.py
@@ -0,0 +1,9 @@
+import customtkinter as ctk
+from customtkinter import *
+
+
+class Main(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.title("Main Page")
+        self.geometry("300x300")
---
+++ view/To_do.py
@@ -0,0 +1,9 @@
+import customtkinter as ctk
+from customtkinter import *
+
+
+class Main(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.title("TODO_LIST")
+        self.geometry("300x300")
